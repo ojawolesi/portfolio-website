6ab74fe9695a6871944a4d4e5c9a60bc2478e63f
ojawolesi <j.awolesi99@gmail.com>
2022-12-08 17:34:01 +0000

    Typewriter effect added and centre intro on mobile view

--- src/components/IntroSection/IntroSection.jsx
+++ src/components/IntroSection/IntroSection.jsx
@@ -3,8 +3,9 @@ import "./IntroSection.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import portraitImg from "../../assets/images/studio-portrait-1.jpg";
 import { Button } from "react-bootstrap";
+import Typewriter from "typewriter-effect";
+import portraitImg from "../../assets/images/studio-portrait-1.jpg";
 
 function IntroSection({ isSmallScreen }) {
   return (
@@ -12,26 +13,62 @@ function IntroSection({ isSmallScreen }) {
       <Container className="mx-auto">
         <Row>
           <Col xs lg="5">
-            <div></div>
-            <div style={{ marginTop: "12rem" }}>
+            <div className={`${isSmallScreen && "m-heading-box"} heading-box`}>
               <h3>Hello, it's me</h3>
               <h1>
                 Olasubomi
                 <span style={{ color: "#5f99ff" }}>.</span>
               </h1>
+              <div className={"typist"}>
+                <Typewriter
+                  options={{
+                    strings: [
+                      "<h1 >Software<br/>Engineer</h1>",
+                      "<h1 >Web<br/>Developer</h1>",
+                      "<h1 >Graphic<br/>Designer</h1>",
+                    ],
+                    autoStart: true,
+                    loop: true,
+                    cursor: "",
+                  }}
+                />
+              </div>
               <p>
                 An enthusiastic and proactive technologist, passionate about all
                 things design. Currently looking for an innovative environment
                 with a positive culture to develop and grow as a full-stack
                 software engineer.
               </p>
-              <Button className="scrollBtn">Scroll for more</Button>
+              {!isSmallScreen && (
+                <Button className="scrollBtn">Scroll for more</Button>
+              )}
             </div>
           </Col>
           <Col lg={5}>
-            <div>
-              <img src={portraitImg} alt="" width="535" />
-            </div>
+            {isSmallScreen ? (
+              <div className="mx-auto md:w-1/2">
+                {isSmallScreen ? (
+                  <img src={portraitImg} alt="" className="" />
+                ) : (
+                  <img src={portraitImg} alt="" width="500" />
+                )}
+              </div>
+            ) : (
+              <div
+                className="mx-auto md:w-1/2"
+                data-aos="zoom-in"
+                data-aos-duration="2000"
+              >
+                {isSmallScreen ? (
+                  <img src={portraitImg} alt="" className="" />
+                ) : (
+                  <img src={portraitImg} alt="" width="500" />
+                )}
+              </div>
+            )}
+            {/*<div>*/}
+            {/*  <img src={portraitImg} alt="" width="535" />*/}
+            {/*</div>*/}
           </Col>
         </Row>
       </Container>
